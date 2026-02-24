07609283676164de6b80e5352449fa7693ce3842
unknown <ikechukwuchiemerie822@gmail.com>
2026-02-24 07:41:05 +0100

    mobile looks a bit cool

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const parentMenu = document.querySelector("#parentMenu");
 const openBtn = document.querySelector("#openBtn");
 const closeBtn = document.querySelector("#closeBtn");
+const bookmarkBtn = document.querySelector("#bookmarkBtn")
+const text = document.querySelector(".text")
 
 openBtn.addEventListener("click", () => {
     parentMenu.classList.add("active")
@@ -9,4 +11,7 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     parentMenu.classList.remove("active")
     openBtn.style.display = "flex"
+})
+bookmarkBtn.addEventListener("click", () => {
+    bookmarkBtn.classList.toggle("active")
 })
