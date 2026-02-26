e932e5056fa9f4d6ffe7eddc57e03a166d7cade7
unknown <ikechukwuchiemerie822@gmail.com>
2026-02-26 22:58:08 +0100

    modal half way good on mobile

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const openBtn = document.querySelector("#openBtn");
 const closeBtn = document.querySelector("#closeBtn");
 const bookmarkBtn = document.querySelector("#bookmarkBtn")
 const text = document.querySelector(".text")
-
+const openBtnProject = document.querySelector("#openBtn-project")
+const parentContainer = document.querySelector("#parent-project-container")
+const btnClosemodal = document.querySelector("#btnClosemodal")
 openBtn.addEventListener("click", () => {
     parentMenu.classList.add("active")
     openBtn.style.display = "none"
@@ -16,4 +18,12 @@ closeBtn.addEventListener("click", () => {
 bookmarkBtn.addEventListener("click", () => {
     bookmarkBtn.classList.toggle("active")
     
+})
+openBtnProject.addEventListener("click", () => {
+    parentContainer.classList.add("active")
+    // document.body.style.overflow = "hidden";
+})
+btnClosemodal.addEventListener("click" , () => {
+    parentContainer.classList.remove("active")
+    // document.body.style.overflow = "auto";
 })
